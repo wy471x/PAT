17b1d5bfe80d628e058e0175baa37147c6196f25
dunk <wy471x@qq.com>
2021-01-27 09:28:54 +0800

    Problem 1050 finishing.

--- 1049.cpp
+++ 1049.cpp
@@ -58,17 +58,17 @@
 //     }
 //     return res;
 // }
-#include<iostream>
+#include <iostream>
 using namespace std;
-double sum[100005];
-int main(){
-	int n;
-	cin>>n;
-	double s=0;
-	for(int i=0;i<n;i++){
-		cin>>sum[i];
-		s+=sum[i]*(i+1)*(n-i);
-	} 
-	printf("%.2f",s);
-	return 0;
-} 
+int main() {
+    int n;
+    cin >> n;
+    long long sum = 0;
+    double temp;
+    for (int i = 1; i <= n; i++) { 
+        cin >> temp;
+        sum += (long long)(temp * 1000) * i * (n - i + 1);
+    }
+    printf("%.2f", sum / 1000.0);
+    return 0;
+}
